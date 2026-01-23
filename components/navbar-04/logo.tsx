1085f751849fe544328bbcc68dda1e69a1aa2a33
eshromanch <romanch.haq868@gmail.com>
2026-01-23 20:50:47 +0600

    Fix: Replace Next.js Image with regular img tags for dynamic content
    - Changed logo to use regular img tag to avoid caching issues
    - Gallery pages already use regular img tags for uploaded images
    - Image uploader components use regular img tags for previews
    - This prevents the need to rebuild after uploading new images

--- components/navbar-04/logo.tsx
+++ components/navbar-04/logo.tsx
@@ -1,9 +1,8 @@
 import { logo } from "@/data/navigationData";
-import Image from "next/image";
 
 export const Logo = () => (
   <a href={logo.href} >
     {/* {logo.text} */}
-    <Image src="/logo.png" alt="Logo" width={500} height={200} className="w-[170px] h-[100px] lg:h-[100px]"/>
+    <img src="/logo.png" alt="Logo" width="500" height="200" className="w-[170px] h-[100px] lg:h-[100px]"/>
   </a>
 );
